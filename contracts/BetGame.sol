df5ddb72ca1b4d30132b88a067792f9f1402af58
sunny0714 <sun2020victory@gmail.com>
2022-02-12 15:27:53 +0200

    add methods

--- contracts/BetGame.sol
+++ contracts/BetGame.sol
@@ -15,9 +15,47 @@ contract Betting {
 
   // structs are custom data structures with self-defined parameters
   struct Bet {
-    uint outcome;
-    uint amount;
+    uint256 outcome;
+    uint256 amount;
     bool initialized;
+  }
+
+  // keep track of every gambler's bet
+  mapping (address => Bet) bets;
+  // keep track of every player's winnings (if any)
+  mapping (address => uint256) winnings;
+  // keep track of all outcomes
+  mapping (uint256 => uint256) public outcomes;
+
+  // Add any events you think are necessary
+  event BetMade(address gambler);
+  event BetClosed();
+
+  // modifier
+  modifier ownerOnly() {_;}
+  modifier oracleOnly() {_;}
+  modifier outcomeExists(uint256 outcome) {_;}
+
+  // Owner chooses their trusted Oracle
+  function chooseOracle(address _oracle) external ownerOnly() returns (address) {
+      oracle = _oracle;
+      return oracle;
+  }
+
+  // Gamblers place their bets, preferably after calling checkOutcomes
+  function makeBet(uint256 _outcome) external payable {
+      Bet memory newBet = Bet(_outcome, msg.value, true);
+      bets[msg.sender] = newBet;
+  }
+
+  // Allow anyone to check the outcomes they can bet on
+  function checkOutcomes(uint outcome) public view returns (uint) {
+      return outcomes[outcome];
+  }
+  
+  // Allow anyone to check if they won any bets
+  function checkWinnings() public view returns(uint) {
+      return winnings[msg.sender];
   }
 
 }
